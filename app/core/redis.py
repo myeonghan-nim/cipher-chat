691c13a61269e33bfa3d09f0e00158322288e5cb
myeonghan-nim <hannim0828@gmail.com>
2025-03-26 14:47:25 +0900

    Change: SSL in application

--- app/core/redis.py
+++ app/core/redis.py
@@ -6,5 +6,12 @@ redis_client = redis.Redis(
     host=os.getenv("REDIS_HOST"),
     port=int(os.getenv("REDIS_PORT")),
     db=int(os.getenv("REDIS_DB")),
+    password=os.getenv("REDIS_PASSWORD"),
     decode_responses=True,
+    ssl=True,
+    ssl_certfile="/etc/ssl/certs/redis.crt",
+    ssl_keyfile="/etc/ssl/private/redis.key",
+    ssl_ca_certs="/etc/ssl/certs/ca.crt",
+    # hostname 검증 옵션
+    ssl_check_hostname=False,
 )
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-uvicorn app.main:app --host $FASTAPI_HOST --port $FASTAPI_PORT
+uvicorn app.main:app \
+        --host $FASTAPI_HOST \
+        --port $FASTAPI_PORT \
+        --ssl-certfile /etc/ssl/certs/fastapi.crt \
+        --ssl-keyfile /etc/ssl/private/fastapi.key
